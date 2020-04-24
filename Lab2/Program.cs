588fd0d1ba517dd7f406e047a6ce09660c1a2ea4
Ihor08 <ihor100500@ukr.net>
2020-04-24 11:33:16 +0300

    first program

---
+++ Lab2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace Lab2
+{
+    class Class1
+    {
+        static void Main()
+        {
+
+
+            Console.WriteLine("Введіть імя одногрупника ");
+
+            string name = Console.ReadLine();
+            switch (name)
+            {
+                case "Галина":
+                    Console.WriteLine("Степанюк");
+                    break;
+                case "Степан":
+                    Console.WriteLine("Мартинюк");
+                    break;
+                case "Роман":
+                    Console.WriteLine("Рибак");
+                    break;
+                case "Максим":
+                    Console.WriteLine("Томин");
+                    break;
+                case "Павло":
+                    Console.WriteLine("Осадчук");
+                    break;
+                case "Влад":
+                    Console.WriteLine("Лях");
+                    break;
+                case "Микита":
+                    Console.WriteLine("Рябов");
+                    break;
+                case "Назар":
+                    Console.WriteLine("Слюсар");
+                    break;
+                case "галина":
+                    Console.WriteLine("Степанюк");
+                    break;
+                case "степан":
+                    Console.WriteLine("Мартинюк");
+                    break;
+                case "роман":
+                    Console.WriteLine("Рибак");
+                    break;
+                case "максим":
+                    Console.WriteLine("Томин");
+                    break;
+                case "павло":
+                    Console.WriteLine("Осадчук");
+                    break;
+                case "влад":
+                    Console.WriteLine("Лях");
+                    break;
+                case "микита":
+                    Console.WriteLine("Рябов");
+                    break;
+                case "назар":
+                    Console.WriteLine("Слюсар");
+                    break;
+
+
+
+            }
+
+        }
+    }
+}
